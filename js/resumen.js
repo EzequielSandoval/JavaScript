fb68427b5ff066f4e54773adab29b23f5192c0d5
EzequielSandoval <sandovaleze3@gmail.com>
2022-02-23 17:38:26 -0300

    js proyecto

--- js/resumen.js
+++ js/resumen.js
@@ -5,6 +5,8 @@ let certOpcion = "NO"
 let respaldo = "Ninguna"
 let codigoDescuento = 0
 let calcDescuentos = 0
+let totalDescuentos = 0
+
 //Calculo impuesto iva
 function impIva(valor) {
     let valorIva = valor * 0.21
@@ -33,20 +35,20 @@ let inputDuracion3 = document.getElementById("duracion3")
 inputDuracion1.onchange = () => {
     duracion = 1;
     costeMeses = parseInt(costePlan)
-    // calcDescuentos = 0
+    updateResumen();
 };
 inputDuracion2.onchange = () => {
     duracion = 6;
     costeMeses = parseInt(costePlan) * duracion;
-    // calcDescuentos = 0
+    updateResumen();
 };
 inputDuracion3.onchange = () => {
     duracion = 12;
     costeMeses = parseInt(costePlan) * duracion;
     Swal.fire('Los planes con duracion de un año poseen un descuento del 25%');
     calcDescuentos = desc25(costeMeses)
-    // costeMeses = costeMeses - calcDescuentos;
     totalDescuentos = calcDescuentos
+    updateResumen();
 };
 
 //precios de los meses segun el plan
@@ -74,16 +76,19 @@ inputSo1.onchange = () => {
             Swal.fire('Agregado!', '', 'success');
             respaldo = "Si";
             costeCopia = 90 * duracion;
+            updateResumen();
         } else if (result.isDenied) {
             Swal.fire('No agregaste la copia de seguridad!', '', 'info');
             respaldo = "No";
 
         }
     })
+    updateResumen();
 };
 inputSo2.onchange = () => {
     sistema = "Windows";
     respaldo = "No";
+    costeCopia = 0;
     Swal.fire({
         title: 'Este sistema no posee copia de seguridad',
         icon: 'info',
@@ -91,7 +96,7 @@ inputSo2.onchange = () => {
         focusConfirm: false,
         confirmButtonText: 'OK',
     })
-
+    updateResumen();
 };
 
 
@@ -101,56 +106,45 @@ inputSsl.onchange = () => {
     if (inputSsl.checked == true) {
         certOpcion = "SI"
         costeCertificado = 1000
+
     } else {
         costeCertificado = 0
         certOpcion = "NO"
     }
+    updateResumen();
 };
 
 
+let codDesc = document.getElementById("aplicarCodigo")
+let inputCode = document.getElementById("codDesc")
 
-
-
-// Codigo de descuento
-let codDesc = document.getElementById('aplicarCodigo');
-let inputCode = document.getElementById('codDesc');
 
 codDesc.onclick = () => {
-
-    // subtotal = costeMeses + costeCopia + costeCertificado
-    // totalImp = impIva(subtotal)
-    // calculoTotal = totalImp + subtotal
-
+    
     if (inputCode.value == "cybermonday") {
-        codigoDescuento = desc25(calculoTotal)
-        totalDescuentos = calcDescuentos + codigoDescuento
-
-
         Swal.fire({
-            title: 'Codigo cybermonday aplicado',
+            title: 'Codigo "cybermonday" aplicado',
             icon: 'success',
             showCloseButton: true,
             focusConfirm: false,
             confirmButtonText: 'OK',
+            
         })
-
-
+        codigoDescuento = desc25(calculoTotal)
+        
     } else if (inputCode.value == "hFnfqu5") {
         codigoDescuento = desc50(calculoTotal)
-        totalDescuentos = calcDescuentos + codigoDescuento
-
         Swal.fire({
-            title: 'Codigo hFnfqu5 aplicado',
+            title: 'Codigo especial "hFnfqu5" aplicado',
             icon: 'success',
             showCloseButton: true,
             focusConfirm: false,
             confirmButtonText: 'OK',
         })
-
     } else {
 
         Swal.fire({
-            title: 'Codigo de descuento incorrecto. No aplica el descuento',
+            title: 'El codigo ingresado no es valido',
             icon: 'error',
             showCloseButton: true,
             focusConfirm: false,
@@ -158,19 +152,13 @@ codDesc.onclick = () => {
         })
 
     }
+    updateResumen();
+}
 
 
-};
+function updateResumen() {
 
 
-// procesamiento de datos/calculos
-// subtotal = costeMeses + costeCopia + costeCertificado
-// totalImp = impIva(subtotal)
-// calculoTotal = totalImp + subtotal
-// precioFinal = calculoTotal - totalDescuentos
-
-let myForm = document.getElementById("formulario");
-myForm.onchange = () => {
     /*Suma totales*/
     totalDescuentos = calcDescuentos + codigoDescuento
     subtotal = costeMeses + costeCopia + costeCertificado
@@ -180,7 +168,6 @@ myForm.onchange = () => {
 
     /*final*/
     precioFinal = calculoTotal - totalDescuentos
-
 
     // ----------------------
     const resumen = []
@@ -203,22 +190,20 @@ myForm.onchange = () => {
 
     resumen.push(new Resumen(planTipo, costeMeses, duracion, sistema, respaldo, costeCopia, certOpcion, costeCertificado))
 
-
-
     const padre = document.getElementById("resumenContainer")
     for (const resumenProductos of resumen) {
 
         let contenedorResumen = document.querySelector(".resumenProductos")
 
         contenedorResumen.innerHTML = `
-                                    <h4 class="resumenTitulo" >     ${resumenProductos.typePlan}:</h4>
-                                    <p>Coste:                       $${resumenProductos.pricePlan}</p>
-                                    <p>Meses de duracion:           ${resumenProductos.durationPlan}</p>   
-                                    <p>Sistema:                     ${resumenProductos.systemPlan}</p>   
-                                    <p>Copia de seguridad:          ${resumenProductos.backupPlan}</p>   
-                                    <p>Coste:                       $${resumenProductos.priceBackup}</p>   
-                                    <p>Certificado SSL:             ${resumenProductos.sslPlan}</p>    
-                                    <p>Coste:                       $${resumenProductos.priceSSL}</p>    
+                                       <h4 class="resumenTitulo" >     ${resumenProductos.typePlan}:</h4>
+                                       <p>Coste:                       $${resumenProductos.pricePlan}</p>
+                                       <p>Meses de duracion:           ${resumenProductos.durationPlan}</p>   
+                                       <p>Sistema:                     ${resumenProductos.systemPlan}</p>   
+                                       <p>Copia de seguridad:          ${resumenProductos.backupPlan}</p>   
+                                       <p>Coste:                       $${resumenProductos.priceBackup}</p>   
+                                       <p>Certificado SSL:             ${resumenProductos.sslPlan}</p>    
+                                       <p>Coste:                       $${resumenProductos.priceSSL}</p>    
                                 `
 
         padre.appendChild(contenedorResumen)
@@ -250,7 +235,7 @@ myForm.onchange = () => {
                                         <p>Total Descuentos:                          $${preciosTotales.totDiscount.toFixed(2)}</p>
                                         <p>Total:                                     $${preciosTotales.total.toFixed(2)}</p>
                                         <p class="precioFinal">Precio final:          $${preciosTotales.finalPrice.toFixed(2)}</p>
-        
+                                
         
                                       `
         padre.appendChild(contenedorTotales)
